efc4b6559d55e47bc7d70074ecdf4b9625baf910
rpr5w285wh-cmyk <rpr5w285wh@privaterelay.appleid.com>
2026-06-15 11:42:35 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service worker for the Just Sitting zazen app.
+// Caches the app shell so it loads fully offline once visited.
+const CACHE = 'zazen-v10';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('./')))
+  );
+});
